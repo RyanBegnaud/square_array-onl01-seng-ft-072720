3eb2114a03de41a159a023fe0664e5aca3a76828
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-29 16:48:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,15 @@
 def square_array(array)
+<<<<<<< HEAD
       new_array = []
       array.each do |square| 
       square2 = square * square
       new_array.push(square2)
     end
   return new_array
+=======
+      square_a = array.each do |square| 
+      square * square
+      return square_a
+    end
+>>>>>>> e4e7ab44e09b54bac20ce784a51b8b1a42a80220
 end
